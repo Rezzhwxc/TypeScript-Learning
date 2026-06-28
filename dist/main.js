8d398520736b6a565ecfe40f3f7e92a486cbd009
Ярек <yarohatsune@gmail.com>
2026-06-28 08:19:49 +0500

    Intetface, type

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,12 @@
 "use strict";
+// КОМПИЛЛИРОВАТЬ ЧЕРЕЗ tsc
+
 let test = 'it works!!';
 alert(test);
+const adress = {
+    whoLives: {
+        name: 'Oleg',
+        nickname: 'ZXC'
+    },
+    city: 'Alyaska'
+};
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,25 @@
 
 let test: string = 'it works!!';
 alert(test);
+
+// Составные типы
+
+interface User{
+    name: string;
+    nickname: string;
+    age?: number;
+}
+
+type Adress = {
+    city: string;
+    street?: string;
+    whoLives: User;
+}
+
+const adress: Adress = {
+    whoLives: {
+        name: 'Oleg',
+        nickname: 'ZXC'
+    },
+    city: 'Alyaska'
+}
