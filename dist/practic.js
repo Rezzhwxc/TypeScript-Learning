a5cb48dc6f0bf7d8d39fa5bf9f091b5368a73480
Ярек <yarohatsune@gmail.com>
2026-07-10 23:43:20 +0500

    start practic CodeMu

---
+++ dist/practic.js
@@ -0,0 +1,9 @@
+"use strict";
+// #1
+const test1 = 'bebbe';
+const test2 = 0.5;
+const test3 = true;
+// #2
+let change = 'abc';
+change = '12';
+console.log(change);
---
+++ src/practic.ts
@@ -0,0 +1,8 @@
+// #1
+const test1: string = 'bebbe';
+
+const test2: number = 0.5;
+
+const test3: boolean = true;
+
+// #2
