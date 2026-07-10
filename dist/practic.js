048a92fcf4f3e0b6c7c6b0c7dd48a83ab733da0d
Ярек <yarohatsune@gmail.com>
2026-07-11 00:00:40 +0500

    1\5

--- dist/practic.js
+++ dist/practic.js
@@ -1,9 +1,19 @@
 "use strict";
 // #1
-const test1 = 'bebbe';
-const test2 = 0.5;
-const test3 = true;
+const strTest = 'bebbe';
+const numTest = 0.5;
+const boolTest = true;
 // #2
-let change = 'abc';
-change = '12';
-console.log(change);
+let test1 = 3;
+let test2 = 'abc';
+console.log('sum = ' + test2 + test1);
+// #3
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let arr2 = [0, 1, 2, 3, 4, 5];
+// #4
+let date = { year: 2025, month: 12, day: 31 };
+date = { year: 2025, month: 12, day: 7 };
+console.log(date);
+// #5
+date.month = 100;
+console.log(date);
--- src/practic.ts
+++ src/practic.ts
@@ -1,8 +1,27 @@
 // #1
-const test1: string = 'bebbe';
+const strTest: string = 'bebbe';
 
-const test2: number = 0.5;
+const numTest: number = 0.5;
 
-const test3: boolean = true;
+const boolTest: boolean = true;
 
 // #2
+let test1: number = 3;
+let test2: string = 'abc';
+
+console.log('sum = ' + test2 + test1)
+
+// #3
+let arr: string[] = ['a', 'b', 'c', 'd', 'e'];
+
+let arr2: Array<number> = [0, 1, 2, 3, 4, 5];
+
+// #4
+let date = {year: 2025, month: 12, day: 31};
+date = {year: 2025, month: 12, day: 7};
+
+console.log(date)
+
+// #5
+date.month = 100;
+console.log(date)
